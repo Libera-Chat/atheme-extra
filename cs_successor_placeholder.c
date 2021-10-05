c0cf7fde491482a802e19dfde9be5a24fd050b8f
jesopo <github@lolnerd.net>
2021-10-05 01:51:56 +0000

    'cs_successor_liberastaff.c' -> 'cs_successor_placeholder.c'

--- cs_successor_placeholder.c
+++ cs_successor_placeholder.c
@@ -2,8 +2,8 @@
  * Copyright (c) 2012 Marien Zwart <marien.zwart@gmail.com>.
  * Rights to this code are as documented in doc/LICENSE.
  *
- * Forces the successor for single-# channels to be libera-staff,
- * if an account by that name exists.
+ * Forces the successor for single-# channels to be
+ * libera-placeholder-account, if an account by that name exists.
  */
 
 #include "lc-compat.h"
@@ -18,7 +18,7 @@ static void channel_pick_successor_hook(hook_channel_succession_req_t *req)
 	if (req->mc->name[0] == '#' && req->mc->name[1] == '#')
 		return;
 
-	/* Use libera-staff if it exists.
+	/* Use ?AAAAAAABB (libera-placeholder-account) if it exists.
 	 * If myuser_find_ext returns NULL the normal successor logic is used.
 	 * If some other user of this hook picked a successor
 	 * we intentionally overrule it.
@@ -38,7 +38,7 @@ static void mod_deinit(module_unload_intent_t intent)
 
 DECLARE_MODULE_V1
 (
-	"libera/cs_successor_liberastaff", MODULE_UNLOAD_CAPABILITY_OK, mod_init, mod_deinit,
-	"$Id: cs_successor_liberastaff.c 65 2012-06-09 12:25:31Z stephen $",
+	"libera/cs_successor_placeholder", MODULE_UNLOAD_CAPABILITY_OK, mod_init, mod_deinit,
+	"$Id: cs_successor_placeholder.c 65 2012-06-09 12:25:31Z stephen $",
 	"libera chat <https://libera.chat>"
 );
