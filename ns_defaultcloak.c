3272e04144d7aa97eac0a268024cee2870067a45
Aaron Jones <me@aaronmdjones.net>
2024-03-26 02:14:05 +0000

    ns_defaultcloak: several minor tweaks
    - Remove asserts
    
      The built-in lengths of nicknames and hostnames already ensure that they
      could not ever trigger.
    
    - Remove inclusion of <stdint.h>
    
      Upstream already includes this file if it is available
    
    - Correct the module name
    
      The name should match the filesystem path it will be located at, because
      this is the name required for the loadmodule line in the config file. It
      would be quite confusing if the name in the config file and the name in
      OperServ MODLIST were different.
    
    - Reformat blocks to use upstream code style
    
    - Use C99 single-line style comments
    
    - Re-wrap lines with command success/error message strings
    
    - Sprinkle some liberal const-correctness everywhere
    
    - Move struct definition to the top of the file
    
    - Indent struct fields consistent with upstream code style
    
    - Move hash initialisation function to the top of the file
    
    - Change argument order on build_cloak() to put the input variable first
    
    - Rename custom functions to insert an "nsdc_" prefix on all of them
    
      This ensures that their generic names could never clash with a function
      declared in upstream at some point
    
    - Use mowgli string functions consistent with the rest of the codebase
    
    - Use correct data type for snprintf(3) when appending the hash to the vhost

--- ns_defaultcloak.c
+++ ns_defaultcloak.c
@@ -1,292 +1,314 @@
 /*
  * Modification of code that is copyright (c) 2005-2006 William Pitcock, et al.
  *
- * Sets usercloak metadata on register and
- * allows opers to change cloaks to the default.
+ * Sets usercloak metadata on register and allows opers to change cloaks to the default
  */
 
 #include "atheme.h"
-#include <assert.h>
-#include <stdint.h>
 
-/* cloak prefix, must be non-empty. */
-#define CLOAK_PREFIX "user/"
-
-/* Colon, five digits, null. */
-#define CLOAK_SUFFIX_LEN 7
-
-static uint32_t hash_iv;
-
-/* Fills "newhost" with the default cloak for the account name given by "src". */
-/* `hostlen` is the length of the buffer pointed to by newhost. It must be at least 12 bytes large. */
-static bool
-build_cloak(char *newhost, size_t hostlen, const char *src)
-{
-	size_t i;
-	unsigned char hash_char;
-	/* FNV-1a hash */
-	uint32_t hash = hash_iv;
-	bool invalidchar = false;
-	const size_t CLOAK_PREFIX_LEN = strlen(CLOAK_PREFIX);
-	assert(hostlen >= CLOAK_SUFFIX_LEN + CLOAK_PREFIX_LEN);
-	/* Safety: ^ assert ensures there's enough space. */
-	strcpy(newhost, CLOAK_PREFIX);
-	/* Index of the last byte written. */
-	i = CLOAK_PREFIX_LEN - 1;
-	/* Temporarily reduce hostlen to be the index of the last byte. */
-	--hostlen;
-	/* Search for invalid characters. This changes copying behavior. */
-	for (const char *p = src; *p != '\0'; ++p)
-	{
-		if (!isalnum((unsigned char)*p) && *p != '-') {
-			invalidchar = true;
-			break;
-		}
-	}
-	/* Copy the cloak and calculate the hash from casefolded characters. */
-	for (; *src != '\0'; ++src)
-	{
-		const int c = ToUpper((unsigned char)*src);
-		if (i < hostlen)
-		{
-			if (isalnum(c))
-				newhost[++i] = *src;
-			else if (c == '-')
-			{
-				if (!invalidchar || newhost[i] != '-')
-					newhost[++i] = '-';
-			}
-			else if (c == '_')
-			{
-				if (newhost[i] != '-')
-					newhost[++i] = '-';
-			}
-		}
-		hash ^= c;
-		hash *= 16777619;
-	}
-	/* Defensive programming assert. This should never trigger for any input. */
-	assert(i <= hostlen);
-	/* Change i to be the number of bytes written. */
-	++i;
-	/* Restore hostlen to be one past the end. */
-	++hostlen;
-	if (i == hostlen)
-		invalidchar = true;
-	else if (i == CLOAK_PREFIX_LEN)
-		/* Yes, you're very clever. Have an easter egg. */
-		i += snprintf(newhost + i, hostlen - i, "...");
-	if (invalidchar)
-	{
-		if (i > hostlen - CLOAK_SUFFIX_LEN)
-			i = hostlen - CLOAK_SUFFIX_LEN;
-		/* Fold hash value to fit in 5 digits. */
-		hash = (hash >> 17) + (hash & 0xffff);
-		snprintf(newhost + i, hostlen - i, ":%05d", hash);
-	}
-	else
-		newhost[i] = '\0';
-	return invalidchar;
-}
+// Cloak prefix, must be non-empty
+#define CLOAK_PREFIX        "user/"
+#define CLOAK_PREFIX_LEN    strlen(CLOAK_PREFIX)
 
 struct setter
 {
-	struct sourceinfo *si;
-	const char *marker; /* Nullable */
-	bool needs_force;
+	struct sourceinfo * si;
+	const char *        marker;
+	bool                needs_force;
 };
 
-static void
-change_vhost(struct myuser *mu, const char* newhost, bool invalidchar, struct setter *setter) {
-	mowgli_node_t *n;
-	struct user *u;
-	char timestring[16];
+static uint32_t hash_iv = 0;
 
-	if (setter) {
-		command_success_nodata(setter->si, _("Assigned default cloak to \2%s\2."), entity(mu)->name);
+static void
+nsdc_init_hash(void)
+{
+	// FNV-0 to calculate an offset that isn't the default one
+	for (const char *p = CLOAK_PREFIX; *p != '\0'; p++)
+	{
+		hash_iv ^= (uint32_t) *p;
+		hash_iv *= 16777619;
+	}
+}
+
+static bool
+nsdc_build_cloak(const char *const restrict accname, char *const restrict newhost, const size_t hostlen)
+{
+	size_t i = (CLOAK_PREFIX_LEN - 1);
+	bool invalidchar = false;
+	uint32_t hash = hash_iv;
+
+	(void) mowgli_strlcpy(newhost, CLOAK_PREFIX, hostlen);
+
+	// Search for invalid characters. This changes copying behavior
+	for (const char *p = accname; *p != '\0'; p++)
+	{
+		if (*p == '-')
+			continue;
+
+		if (isalnum((unsigned char) *p))
+			continue;
+
+		invalidchar = true;
+		break;
+	}
+
+	// Copy the cloak and calculate the hash from casefolded characters
+	for (const char *p = accname; *p != '\0'; p++)
+	{
+		const int c = ToUpper((unsigned char) *p);
+
+		hash ^= (uint32_t) c;
+		hash *= 16777619;
+
+		if (i >= (hostlen - 1))
+			continue;
+
+		if (isalnum(c))
+		{
+			newhost[++i] = *p;
+		}
+		else if (c == '-')
+		{
+			if ((! invalidchar) || (newhost[i] != '-'))
+				newhost[++i] = '-';
+		}
+		else if (c == '_')
+		{
+			if (newhost[i] != '-')
+				newhost[++i] = '-';
+		}
+	}
+
+	// Bump i to denote the number of bytes written
+	i++;
+
+	if (i == CLOAK_PREFIX_LEN)
+	{
+		// Yes, you're very clever. Have an easter egg
+		(void) mowgli_strlcpy(newhost + i, "...", hostlen - i);
+
+		i += 3;
+	}
+
+	if (invalidchar)
+	{
+		// Fold hash value to fit in 5 digits
+		hash = (hash >> 17) + (hash & 0xFFFF);
+
+		(void) snprintf(newhost + i, hostlen - i, ":%05" PRIu32, hash);
+	}
+	else
+		newhost[i] = '\0';
+
+	return invalidchar;
+}
+
+static void
+nsdc_change_vhost(struct myuser *const restrict mu, const char *const restrict newhost, const bool invalidchar,
+                  const struct setter *const restrict setter)
+{
+	char timestring[16];
+	mowgli_node_t *n;
+
+	if (setter)
+	{
+		(void) command_success_nodata(setter->si, _("Assigned default cloak to \2%s\2."), entity(mu)->name);
+
 		if (setter->needs_force)
 		{
-			wallops("\2%s\2 reset vhost of the \2MARKED\2 account %s.", get_oper_name(setter->si), entity(mu)->name);
-			if (setter->marker) {
-				command_success_nodata(setter->si, _("Overriding MARK placed by %s on the account %s."), setter->marker, entity(mu)->name);
-			} else {
-				command_success_nodata(setter->si, _("Overriding MARK(s) placed on the account %s."), entity(mu)->name);
-			}
+			(void) wallops("\2%s\2 reset vhost of the \2MARKED\2 account %s.",
+			               get_oper_name(setter->si), entity(mu)->name);
+
+			if (setter->marker)
+				(void) command_success_nodata(setter->si,
+				                              _("Overriding MARK placed by %s on the account %s."),
+				                              setter->marker, entity(mu)->name);
+			else
+				(void) command_success_nodata(setter->si,
+				                              _("Overriding MARK(s) placed on the account %s."),
+				                              entity(mu)->name);
 		}
-		logcommand(setter->si, CMDLOG_ADMIN, "DEFAULTCLOAK: \2%s\2", entity(mu)->name);
-		metadata_add(mu, "private:usercloak-assigner", get_source_name(setter->si));
+
+		(void) metadata_add(mu, "private:usercloak-assigner", get_source_name(setter->si));
+		(void) logcommand(setter->si, CMDLOG_ADMIN, "DEFAULTCLOAK: \2%s\2", entity(mu)->name);
 	}
 
-	snprintf(timestring, 16, "%lu", (unsigned long)time(NULL));
-	metadata_add(mu, "private:usercloak-timestamp", timestring);
-	metadata_add(mu, "private:usercloak", newhost);
-	if (nicksvs.me != NULL)
-	{
-		myuser_notice(nicksvs.nick, mu, "You have been given a default cloak.");
-		if (invalidchar)
-			myuser_notice(nicksvs.nick, mu, "Your account name cannot be used in a cloak directly. To ensure uniqueness, a number was added.");
-	}
+	(void) snprintf(timestring, sizeof timestring, "%lu", (unsigned long) time(NULL));
+	(void) metadata_add(mu, "private:usercloak-timestamp", timestring);
+	(void) metadata_add(mu, "private:usercloak", newhost);
+
+	(void) myuser_notice(nicksvs.nick, mu, "You have been given a default user cloak.");
+
+	if (invalidchar)
+		(void) myuser_notice(nicksvs.nick, mu, "Your account name cannot be used in a cloak directly. "
+		                                       "To ensure uniqueness, a number was added.");
+
 	MOWGLI_ITER_FOREACH(n, mu->logins.head)
-	{
-		u = n->data;
-		user_sethost(nicksvs.me->me, u, newhost);
-	}
+		(void) user_sethost(nicksvs.me->me, n->data, newhost);
 }
 
 static void
-handle_verify_register(struct hook_user_req *req)
+nsdc_user_verify_register(struct hook_user_req *const restrict req)
 {
 	char newhost[HOSTLEN + 1];
-	struct myuser *mu = req->mu;
-	bool cloaktype = build_cloak(newhost, sizeof newhost, entity(mu)->name);
-	change_vhost(mu, newhost, cloaktype, NULL);
+	const bool cloaktype = nsdc_build_cloak(entity(req->mu)->name, newhost, sizeof newhost);
+
+	(void) nsdc_change_vhost(req->mu, newhost, cloaktype, NULL);
 }
 
 static void
-ns_cmd_defaultcloak(struct sourceinfo *si, int parc, char *parv[])
+ns_cmd_defaultcloak_func(struct sourceinfo *const restrict si, const int parc, char **const restrict parv)
 {
 	struct setter setter = {
-		.si = si,
-		.marker = NULL,
-		.needs_force = false
+		.si             = si,
+		.marker         = NULL,
+		.needs_force    = false,
 	};
-	struct myuser *mu;
-	struct metadata *md;
-	int cloaktype;
-	char newhost[HOSTLEN + 1];
-	struct hook_user_needforce needforce_hdata;
-	bool force = false;
-	bool missingforce = false;
-	char *target = parv[0];
 
-	/* Parse arguments. */
-	if (!target)
+	const struct metadata *md;
+	bool missingforce = false;
+	char newhost[HOSTLEN + 1];
+	bool force = false;
+	struct myuser *mu;
+
+	if (! parv[0])
 	{
-		command_fail(si, fault_needmoreparams, STR_INSUFFICIENT_PARAMS, "DEFAULTCLOAK");
-		command_fail(si, fault_needmoreparams, _("Syntax: DEFAULTCLOAK <account> [FORCE|SHOW]"));
+		(void) command_fail(si, fault_needmoreparams, STR_INSUFFICIENT_PARAMS, "DEFAULTCLOAK");
+		(void) command_fail(si, fault_needmoreparams, _("Syntax: DEFAULTCLOAK <account> [FORCE|SHOW]"));
+
 		return;
 	}
-	if (!(mu = myuser_find_ext(target)))
+
+	if (! (mu = myuser_find_ext(parv[0])))
 	{
-		command_fail(si, fault_nosuch_target, STR_IS_NOT_REGISTERED, target);
+		(void) command_fail(si, fault_nosuch_target, STR_IS_NOT_REGISTERED, parv[0]);
+
 		return;
 	}
+
+	const bool cloaktype = nsdc_build_cloak(entity(mu)->name, newhost, sizeof newhost);
+
 	if (parv[1])
 	{
-		if(!strcasecmp(parv[1], "SHOW"))
+		if (strcasecmp(parv[1], "SHOW") == 0)
 		{
-			build_cloak(newhost, sizeof newhost, entity(mu)->name);
-			command_success_nodata(si, _("Default cloak for \2%s\2: %s"), entity(mu)->name, newhost);
+			(void) command_success_nodata(si, _("Default cloak for \2%s\2: %s"),
+			                                    entity(mu)->name, newhost);
+
 			return;
 		}
-		else if(strcasecmp(parv[1], "FORCE"))
+		if (strcasecmp(parv[1], "FORCE") != 0)
 		{
-			command_fail(si, fault_badparams, _("Invalid keyword \2%s\2."), parv[1]);
-			command_fail(si, fault_badparams, _("Syntax: DEFAULTCLOAK <account> [FORCE|SHOW]"));
+			(void) command_fail(si, fault_badparams, _("Invalid keyword \2%s\2."), parv[1]);
+			(void) command_fail(si, fault_badparams, _("Syntax: DEFAULTCLOAK <account> [FORCE|SHOW]"));
+
 			return;
 		}
+
 		force = true;
 	}
 
-	/* Make default cloak, check if target already has it. */
-	cloaktype = build_cloak(newhost, sizeof newhost, entity(mu)->name);
-	md = metadata_find(mu, "private:usercloak");
-	if (md && !strcmp(md->value, newhost))
+	if ((md = metadata_find(mu, "private:usercloak")) && strcmp(md->value, newhost) == 0)
 	{
-		command_fail(si, fault_nochange, _("\2%s\2 already has a default cloak."), entity(mu)->name);
+		(void) command_fail(si, fault_nochange, _("\2%s\2 already has a default cloak."), entity(mu)->name);
+
 		return;
 	}
 
-	/* Require force for resetting your own cloak. */
-	if (!force && mu == si->smu)
+	// Require force for resetting your own cloak
+	if (mu == si->smu && ! force)
 	{
-		command_fail(si, fault_badparams, _("You are attempting to set your own cloak to default."));
-		command_fail(si, fault_badparams, _("Add %s to confirm you want to do this."), "FORCE");
-		logcommand(si, CMDLOG_ADMIN, "failed DEFAULTCLOAK \2%s\2 (self)", entity(mu)->name);
+		(void) command_fail(si, fault_badparams, _("You are attempting to set your own cloak to default."));
+		(void) command_fail(si, fault_badparams, _("Add %s to confirm you want to do this."), "FORCE");
+
 		return;
 	}
 
-	/* Check if force is required due to marks. */
+	// Check if force is required due to marks
 	if ((md = metadata_find(mu, "private:mark:setter")))
 	{
-		setter.needs_force = true;
 		setter.marker = md->value;
+		setter.needs_force = true;
 	}
 	else
 	{
-		needforce_hdata.si = si;
-		needforce_hdata.mu = mu;
-		needforce_hdata.allowed = 1;
-		hook_call_user_needforce(&needforce_hdata);
+		struct hook_user_needforce needforce_hdata = {
+			.si         = si,
+			.mu         = mu,
+			.allowed    = 1,
+		};
+
+		(void) hook_call_user_needforce(&needforce_hdata);
+
 		setter.needs_force = !needforce_hdata.allowed;
 	}
 
-	/* Check permissions and force if required due to marks. */
+	// Check permissions and force if required due to marks
 	if (setter.needs_force)
 	{
-		if (!has_priv(si, PRIV_MARK))
+		if (! has_priv(si, PRIV_MARK))
 		{
 			missingforce = true;
-			command_fail(si, fault_noprivs, STR_NO_PRIVILEGE, PRIV_MARK);
+
+			(void) command_fail(si, fault_noprivs, STR_NO_PRIVILEGE, PRIV_MARK);
 		}
 		else if (!force)
 		{
 			missingforce = true;
+
 			if (setter.marker)
-				command_fail(si, fault_badparams, _("This operation cannot be performed on %s, because the account has been marked by %s."), entity(mu)->name, setter.marker);
+				(void) command_fail(si, fault_badparams, _("This operation cannot be performed on "
+				                                           "%s, because the account has been marked "
+				                                           "by %s."), entity(mu)->name,
+				                                           setter.marker);
 			else
-				command_fail(si, fault_badparams, _("This operation cannot be performed on %s, because the account has been marked."), entity(mu)->name);
-			command_fail(si, fault_badparams, _("Add %s to override this restriction."), "FORCE");
+				(void) command_fail(si, fault_badparams, _("This operation cannot be performed on %s, "
+				                                           "because the account has been marked."),
+				                                           entity(mu)->name);
+
+			(void) command_fail(si, fault_badparams, _("Add %s to override this restriction."), "FORCE");
 		}
 	}
+
 	if (missingforce)
 	{
-			if (setter.marker)
-				logcommand(si, CMDLOG_ADMIN, "failed DEFAULTCLOAK \2%s\2 (marked by \2%s\2)", entity(mu)->name, setter.marker);
-			else
-				logcommand(si, CMDLOG_ADMIN, "failed DEFAULTCLOAK \2%s\2 (marked)", entity(mu)->name);
-			return;
+		if (setter.marker)
+			(void) logcommand(si, CMDLOG_ADMIN, "failed DEFAULTCLOAK \2%s\2 (marked by \2%s\2)",
+			                                    entity(mu)->name, setter.marker);
+		else
+			(void) logcommand(si, CMDLOG_ADMIN, "failed DEFAULTCLOAK \2%s\2 (marked)", entity(mu)->name);
+
+		return;
 	}
 
-	change_vhost(mu, newhost, cloaktype, &setter);
+	(void) nsdc_change_vhost(mu, newhost, cloaktype, &setter);
 }
 
-static struct command ns_defaultcloak = {
+static struct command ns_cmd_defaultcloak = {
 	.name           = "DEFAULTCLOAK",
 	.desc           = N_("Resets cloaks to default."),
 	.access         = PRIV_USER_VHOST,
 	.maxparc        = 2,
-	.cmd            = &ns_cmd_defaultcloak,
+	.cmd            = &ns_cmd_defaultcloak_func,
 	.help           = { .path = "nickserv/defaultcloak" },
 };
-
-static void
-init_hash(void)
-{
-	const char *p = CLOAK_PREFIX;
-	/* FNV-0 to calculate an offset that isn't the default one. */
-	hash_iv = 0;
-	while (*p != 0)
-	{
-		hash_iv ^= *p++;
-		hash_iv *= 16777619;
-	}
-}
 
 static void
 mod_init(struct module *const restrict m)
 {
 	MODULE_TRY_REQUEST_DEPENDENCY(m, "nickserv/vhost");
-	init_hash();
-	hook_add_user_verify_register(handle_verify_register);
-	service_named_bind_command("nickserv", &ns_defaultcloak);
+
+	(void) nsdc_init_hash();
+
+	(void) hook_add_user_verify_register(&nsdc_user_verify_register);
+	(void) service_named_bind_command("nickserv", &ns_cmd_defaultcloak);
 }
 
 static void
 mod_deinit(const enum module_unload_intent ATHEME_VATTR_UNUSED intent)
 {
-	hook_del_user_verify_register(handle_verify_register);
-	service_named_unbind_command("nickserv", &ns_defaultcloak);
+	(void) hook_del_user_verify_register(&nsdc_user_verify_register);
+	(void) service_named_unbind_command("nickserv", &ns_cmd_defaultcloak);
 }
 
-SIMPLE_DECLARE_MODULE_V1("nickserv/defaultcloak", MODULE_UNLOAD_CAPABILITY_OK)
+SIMPLE_DECLARE_MODULE_V1("freenode/ns_defaultcloak", MODULE_UNLOAD_CAPABILITY_OK)
