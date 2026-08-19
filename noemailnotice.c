22bcefe2e7a15bcd6157b54cbb55a06515e1ea84
Ryan Schmidt <skizzerz@skizzerz.net>
2026-08-18 20:48:27 -0700

    Update hook signature
    Upstream atheme/atheme@88de242 adjusted the signature for this hook.

--- noemailnotice.c
+++ noemailnotice.c
@@ -9,7 +9,7 @@
 #include "fn-compat.h"
 #include "atheme.h"
 
-static void user_identify_notice(user_t *u);
+static void user_identify_notice(struct hook_user_identify *hdata);
 
 static void mod_init(module_t *m)
 {
@@ -21,19 +21,19 @@ static void mod_deinit(module_unload_intent_t intentvoid)
 	hook_del_user_identify(user_identify_notice);
 }
 
-static void user_identify_notice(user_t *u)
+static void user_identify_notice(struct hook_user_identify *hdata)
 {
-	myuser_t *mu = u->myuser;
+	myuser_t *mu = hdata->u->myuser;
 	if (mu->flags & MU_WAITAUTH)
 	{
 		return;
 	}
 	if (!validemail(mu->email))
 	{
-		notice(nicksvs.nick, u->nick, "WARNING: Your NickServ account does not have a valid email address set.");
-		notice(nicksvs.nick, u->nick, "Should you forget your password it may not be possible to recover your acccount.");
-		notice(nicksvs.nick, u->nick, "For help setting an email address, see \2/msg NickServ HELP SET EMAIL\2.");
-		notice(nicksvs.nick, u->nick, "Should you need more assistance you can /join #freenode to find network staff.");
+		notice(nicksvs.nick, hdata->u->nick, "WARNING: Your NickServ account does not have a valid email address set.");
+		notice(nicksvs.nick, hdata->u->nick, "Should you forget your password it may not be possible to recover your acccount.");
+		notice(nicksvs.nick, hdata->u->nick, "For help setting an email address, see \2/msg NickServ HELP SET EMAIL\2.");
+		notice(nicksvs.nick, hdata->u->nick, "Should you need more assistance you can /join #freenode to find network staff.");
 	}
 }
 
